2a9f4b431871d21b7c951884bca02a05f756b419
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-05-10 14:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function stringChop(str, size) {
-  // your code here
+function chunkString(str, size) {
+  // Initialize an empty array to store the chunks
+  const chunks = [];
+  
+  // Loop through the string, incrementing by the specified chunk size
+  for (let i = 0; i < str.length; i += size) {
+    // Slice the string from the current index to the index + size
+    // and push the sliced chunk into the chunks array
+    chunks.push(str.slice(i, i + size));
+  }
+  
+  // Return the array of chunks
+  return chunks;
 }
 
 // Do not change the code below
