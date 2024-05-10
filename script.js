d033f190b9d29b4ad8c2fdef76ac09cf9642f4ca
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-05-10 19:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 function stringChop(str, size) {
-  // Check if the input string is null or empty
-  if (!str) {
-    return [];
-  }
-
-  // Initialize an empty array to store the chunks
-  const chunks = [];
-
-  // Loop through the string, incrementing by the specified chunk size
-  for (let i = 0; i < str.length; i += size) {
-    // Slice the string from the current index to the index + size
-    // and push the sliced chunk into the chunks array
-    chunks.push(str.slice(i, i + size));
-  }
-
-  // Return the array of chunks
-  return chunks; 
+  
 }
 
 // Do not change the code below
