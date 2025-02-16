5dc83cda5e0daebe94afc270ca8f0dd574f632f5
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2025-02-16 18:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function stringChop(str, size) {
-	if(!str) return []
-	
-	let chunks =[];
-	for(let i=0;i<str.length;i = i+size){
-		let subStr = str.slice(i, i+size);
-		chunks.push(subStr);
+	if(!str) return [];
+	let chunks = [];
+	for(let i = 0; i < str.length; i += size){
+		let subStr = str.slice(i, i + size);
+		chunks.push(subStr)
 	}
-
-	return chunks;
 }
 
 console.log(stringChop("Hello, world!", 5)); 
